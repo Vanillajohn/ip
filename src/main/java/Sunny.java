26d55eb813248c66e04d477b88eac15b69e96ad6
Aidan Chiang <aidanchiang@gmail.com>
2026-08-17 14:51:45 +0800

    Add Echo: fulfil Level-1 requirements by adding echo functionality

--- src/main/java/Sunny.java
+++ src/main/java/Sunny.java
@@ -1,15 +1,31 @@
 import java.util.Random;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sunny {
         public static void main(String[] args) {
 
         List<String> greetings = List.of("Yeah, it's me, Sunny.\nWhat do you want?", "What now?\nCan you go bother someone else?");
         List<String> goodbyes = List.of("Ugh, I can't believe I helped you.", "See you never.");
-        
+        List<String> echoRemarks = List.of("", "", "Is this funny to you?", "What a waste of time.");
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         speak(greetings);
+        System.out.println("____________________________________________________________");
+
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                scanner.close();
+                break;
+            }
+            System.out.println(input);
+            speak(echoRemarks);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
         speak(goodbyes);
         System.out.println("____________________________________________________________");
     }
@@ -17,7 +33,6 @@ public class Sunny {
     private static void speak(List<String> greetings) {
             Random rand = new Random();
             int index = rand.nextInt(greetings.size());
-            System.out.println("____________________________________________________________");
             System.out.println(greetings.get(index));
     }
 }
