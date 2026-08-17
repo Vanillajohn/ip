e81552d7937fd3ae59f9dacdacc957cd2a83a310
Aidan Chiang <aidanchiang@gmail.com>
2026-08-17 14:31:34 +0800

    Add greeting: fulfil the level 0 requirements by adding a greeting and exits

--- src/main/java/Sunny.java
+++ src/main/java/Sunny.java
@@ -1,10 +1,23 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Sunny {
-    public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        public static void main(String[] args) {
+
+        List<String> greetings = List.of("Yeah, it's me, Sunny.\nWhat do you want?", "What now?\nCan you go bother someone else?");
+        List<String> goodbyes = List.of("Ugh, I can't believe I helped you.", "See you never.");
+        
+        System.out.println("____________________________________________________________");
+        speak(greetings);
+        speak(goodbyes);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void speak(List<String> greetings) {
+            Random rand = new Random();
+            int index = rand.nextInt(greetings.size());
+            System.out.println("____________________________________________________________");
+            System.out.println(greetings.get(index));
     }
 }
