12861661982a25386b807609c2e19860adca1fd1
Aidan Chiang <aidanchiang@gmail.com>
2026-08-22 15:10:20 +0800

    Add date and time functionality
    Previous system saw dates and time as mere strings and lacked
    functionality that actual dates and time would have.
    
    Let's save dates and time as LocalDateTime objects from Java's library,
    while also ensuring custom text that are obviously not dates be allowed
    for user customizability.
    
    This would provide such functionality that previously wasn't there,
    while preserving user ability to use whatever they wish for dates.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,31 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDateTime by;
+    protected String notDate;
+    private final DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    public Deadline(String description, LocalDateTime by, String notDate) {
         super(description);
         this.by = by;
+        this.notDate = notDate;
     }
 
     public String getFileFormat(){
-        return " | " + this.by + " | " + (this.isDone() ? "1" : "0");
+        if (this.notDate.equals("")){
+            return " | " + this.by.format(customFormatter) + " | " + (this.isDone() ? "1" : "0");
+        } else {
+            return " | " + this.notDate + " | " + (this.isDone() ? "1" : "0");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        if (this.notDate.equals("")){
+            return "[D]" + super.toString() + " (by: " + this.by.format(customFormatter) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + this.notDate + ")";
+        }
     }
 }
